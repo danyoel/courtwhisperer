0dd2456df06fc6363249c5ca37a58dd4a1e91d0d
danyoel <dan.liebling@gmail.com>
2015-11-06 21:48:18 -0800

    adding web api project

---
+++ restapi/App_Code/FormController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+
+public class FormController : ApiController
+{
+    // GET api/<controller>
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<controller>/5
+    public string Get(int id)
+    {
+        return "hello, world!";
+    }
+
+    // POST api/<controller>
+    public string Post(JObject form)
+    {
+        return "success";
+    }
+}
