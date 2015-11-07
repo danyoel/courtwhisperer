e8e5f52c5ec69c0ac06acafb48ea13e53ff5a6ae
danyoel <dan.liebling@gmail.com>
2015-11-06 22:37:03 -0800

    adding most basic word wrapper

---
+++ formfiller/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+
+namespace formfiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new Application();
+            object docname = "";
+            var doc = app.Documents.Open(ref docname);
+                        
+        }
+    }
+}
