df5b7c3cd95c4a767b097c2deeb43ad264165e66
danyoel <dan.liebling@gmail.com>
2016-02-06 17:14:24 -0800

    rework for demo day

--- formfiller/BookmarkLookup.cs
+++ formfiller/BookmarkLookup.cs
@@ -9,34 +9,49 @@ namespace formfiller
 {
     class BookmarkLookup
     {
-        public static string Value(JObject data, string bookmarkName)
+        public static string Value(Dictionary<string, string> fields, string bookmarkName)
         {
+            DateTime date;
+            String text;
+
             switch (bookmarkName)
             {
                 case "Petitioner1":
                 case "Petitioner2":
-                    return String.Format("{0} {1}", data["petitioner"]["firstName"], data["petitioner"]["lastName"]);
+                    return fields.TryGetValue("petitioner-name", out text) ? text : null;
                 case "Respondent1":
                 case "Respondent2":
-                    return String.Format("{0} {1}", data["respondent"]["firstName"], data["respondent"]["lastName"]);
+                    return fields.TryGetValue("respondent-name", out text) ? text : null;
                 case "BirthdatePet1":
-                    return data["petitioner"]["birthDate"].Value<string>().Substring(0, 10);
+                    return fields.TryGetValue("petitioner-birth-date", out text) ? text.Substring(0, 10) : null;
                 case "BirthdateRep2":
-                    return data["respondent"]["birthDate"].Value<string>().Substring(0, 10);
+                    return fields.TryGetValue("respondent-birth-date", out text) ? text.Substring(0, 10) : null;
                 case "ResidencePet":
-                    return String.Format("{0}, {1}", data["petitioner"]["lastKnownCounty"], data["petitioner"]["lastKnownState"]);
+                    return fields.TryGetValue("petitioner-location", out text) ? text : null;
                 case "ResidenceResp":
-                    return String.Format("{0}, {1}", data["respondent"]["lastKnownCounty"], data["respondent"]["lastKnownState"]);
+                    return fields.TryGetValue("respondent-location", out text) ? text : null;
                 case "dateofmarriage":
-                    return data["dateMarried"].Value<string>().Substring(0, 10);
+                    return fields.TryGetValue("marriage-date", out text) ? text.Substring(0, 10) : null;
                 case "citystatemarriage":
-                    return String.Format("{0}, {1}", data["cityMarried"], data["stateMarried"]);
-
-                case "kidsyesno":
+                    return fields.TryGetValue("marriage-location", out text) ? text : null;
+                /*case "sharedkidsyes":
+                    return jdata.Value<bool>("children") ? " " : "X";
+                case "sharedkidsno":
+                    return jdata.Value<bool>("children") ? "X" : " ";*/
                 case "sharedkidname1":
+                    return fields.TryGetValue("dual-children-0-name", out text) ? text : null;
                 case "sharedkidage1":
+                    if (fields.TryGetValue("dual-children-0-dob", out text) && DateTime.TryParse(text, out date))
+                        return Math.Floor((DateTime.Now - date).TotalDays / 365).ToString();
+                    else
+                        return "";
                 case "sharedkidname2":
+                    return fields.TryGetValue("dual-children-1-name", out text) ? text : null;
                 case "sharedkidage2":
+                    if (fields.TryGetValue("dual-children-1-dob", out text) && DateTime.TryParse(text, out date))
+                        return Math.Floor((DateTime.Now - date).TotalDays / 365).ToString();
+                    else
+                        return "";
                 case "Name1":
                 case "petkidname1":
                 case "petkidage1":
--- formfiller/Program.cs
+++ formfiller/Program.cs
@@ -1,4 +1,5 @@
-﻿/*
+﻿#define WORD
+/*
  * Form Filler: fills in a Word Doc whose bookmarks correspond to property names in a
  * JSON blob retrieved from Azure blob storage.
  *
@@ -25,61 +26,83 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Blob;
 using static System.Console;
+using Microsoft.ServiceBus.Messaging;
 
 namespace formfiller
 {
     static class Program
     {
+        static object docname = @"c:\src\legalvoice\formfiller\dissowithmarkup.docx";
+
         static void Main(string[] args)
         {
-            WriteLine("opening word");
+            var qclient = QueueClient.CreateFromConnectionString(ConfigurationManager.AppSettings["serviceBus"]);
+            qclient.OnMessage(ProcessMessage);
+
+            Console.WriteLine("press enter to exit.");
+            Console.ReadLine();
+        }
+
+
+        static void ProcessMessage(BrokeredMessage msg)
+        {
             var app = new Application();
+            object filename = null;
+
+            WriteLine("queue received message");
+            var blobID = msg.GetBody<string>();
+            msg.Complete();
+
+            WriteLine($"getting blob {blobID}");
+            var latest = GetBlob(blobID);
+            WriteLine(latest);
+
+            WriteLine("opening word");
+#if WORD
             try
             {
-                object docname = @"c:\src\legalvoice\formfiller\dissowithmarkup.docx";
                 var doc = app.Documents.Open(ref docname);
 
-                WriteLine("Press ENTER to read the posted data.");
-                ReadLine();
+                WriteLine("filling in form");
+                var fields =
+                    latest["fields"]
+                    .Where(o => o.Value<string>("response") != null)
+                    .ToDictionary(o => o.Value<string>("id"), o => o.Value<string>("response"));
+                FillForm(fields, doc);
 
-                WriteLine("getting latest blob");
-                var latest = TTask.Run(async () => await GetLatestBlobAsync()).Result;
+                /*WriteLine("exporting PDF");
+                var pdf = ExportPDF(doc);
+                WriteLine("opening PDF");
+                OpenPDF(pdf);*/
 
-                if (latest == null)
-                {
-                    WriteLine("!! no blobs to get");
-                }
-                else
-                {
-                    WriteLine("filling in form");
-                    FillForm(latest, doc);
-
-                    WriteLine("exporting PDF");
-                    var pdf = ExportPDF(doc);
-                    WriteLine("opening PDF");
-                    OpenPDF(pdf);
-                }
-
+                filename = Path.ChangeExtension(Path.GetTempFileName(), ".docx");
+                WriteLine($"saving to {filename}");
+                doc.SaveAs2(ref filename);
                 object saveChanges = false;
                 doc.Close(ref saveChanges);
+            }
+            catch (Exception e)
+            {
+                WriteLine(e.ToString());
             }
             finally
             {
                 app.Quit();
             }
 
-            /*var latest = TTask.Run(async () => await GetLatestBlobAsync()).Result;
-            Console.WriteLine(latest.ToString(Formatting.Indented));
-            Console.ReadLine();*/
+            if (filename != null)
+                Process.Start(new ProcessStartInfo((string)filename) { UseShellExecute = true });
+#endif
         }
 
 
 
-        private static void FillForm(JObject latest, Document doc)
+        private static void FillForm(Dictionary<string, string> fields, Document doc)
         {
+            var bmc = doc.Bookmarks.Cast<Bookmark>().Count();
             foreach (var bm in doc.Bookmarks.Cast<Bookmark>()) {
                 //var prop = latest.Property(bm.Name);
-                var value = BookmarkLookup.Value(latest, bm.Name);
+                var value = BookmarkLookup.Value(fields, bm.Name);
 
                 if (value != null)
                 {
@@ -106,22 +129,38 @@ namespace formfiller
         }
 
 
-        static async Task<JObject> GetLatestBlobAsync()
+        static  JObject GetLatestBlob()
         {
             var client = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["connectionString"])
                 .CreateCloudBlobClient();
 
             // extremely inefficient. good enough for the hackathon.
             var cont = client.GetContainerReference("disso");
-            var blobs = await cont.ListBlobsSegmentedAsync(null);
+            var blobs = cont.ListBlobsSegmented(null);
             var latest = blobs.Results.OfType<CloudBlob>().OrderByDescending(b => b.Properties.LastModified).FirstOrDefault();
             if (latest == null)
                 return null;
 
-            var stream = await latest.OpenReadAsync();
+            var stream = latest.OpenRead();
             using (var reader = new StreamReader(stream))
             {
                 return (JObject) JObject.ReadFrom(new JsonTextReader(reader));
+            }
+        }
+
+
+        static JObject GetBlob(string id)
+        {
+            var client = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["connectionString"])
+                .CreateCloudBlobClient();
+            //var cont = client.GetContainerReference("disso");
+            //var blob = cont.GetBlobReference(id);
+            var blob = client.GetBlobReferenceFromServer(new StorageUri(new Uri(id)));
+            using (var ms = new MemoryStream())
+            {
+                blob.DownloadToStream(ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                return JObject.Load(new JsonTextReader(new StreamReader(ms)));
             }
         }
     }
